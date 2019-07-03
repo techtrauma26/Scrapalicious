edc21958099460c87ed78ef7832f2b12c44cdb5a
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-03 03:01:30 -0400

    schema update

---
+++ models/note.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  title: String,
+  body: String
+});
+
+const Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
