13ae2ec989c2252c885794778b642e04be5ea1db
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-08 22:16:08 -0400

    edited port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.engine("handlebars", exphbs({
   defaultLayout: "main",
